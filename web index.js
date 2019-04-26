06c9d4e17852eac70fa18686d2e7ecf58abc6434
abdulahad271 <ahadabdul271@gmail.com>
2019-04-26 22:44:37 +0600

    my first website

---
+++ web index.js
@@ -0,0 +1,106 @@
+	//---type.js---//
+		
+var typed = new Typed(".type", {
+  strings: 
+          [" Freelancer.",
+     	   "Web Designer.",
+	       "Web Developer.",
+			  ],
+           typeSpeed: 40,
+		   backSpeed: 40,
+		   backDelay: 2000,
+		   loop: true,
+});
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  AOS.init();
+  
+
+  (function(){
+
+    'use strict';
+
+
+    var $projects = $('.projects');
+
+    $projects.isotope({
+        itemSelector: '.item',
+        layoutMode: 'fitRows'
+    });
+
+    $('ul.filters > li').on('click', function(e){
+
+        e.preventDefault();
+
+        var filter = $(this).attr('data-filter');
+
+        $('ul.filters > li').removeClass('active');
+        $(this).addClass('active');
+
+        $projects.isotope({filter: filter});
+
+    });
+
+    $('.card').mouseenter(function(){
+
+        $(this).find('.card-overlay').css({'top': '-100%'});
+        $(this).find('.card-hover').css({'top':'0'});
+
+    }).mouseleave(function(){
+
+        $(this).find('.card-overlay').css({'top': '0'});
+        $(this).find('.card-hover').css({'top':'100%'});
+
+    });
+
+})(jQuery);
+  
+  
+  $(function(){
+
+  $('.circlechart').circlechart();
+
+ 
+
+});
+
+  
+  
+  
+  
